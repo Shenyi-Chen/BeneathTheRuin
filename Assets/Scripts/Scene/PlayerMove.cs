85081302e5cec4ecba6419b5f7dfcbc8c479a98f
Shenyi-Chen <sophie.chen.cn@outlook.com>
2025-11-11 06:38:38 +0800

    Animation update

--- Assets/Scripts/Scene/PlayerMove.cs
+++ Assets/Scripts/Scene/PlayerMove.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float speed;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
